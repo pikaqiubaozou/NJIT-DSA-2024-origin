2e41658fadcce1f891f0556824b43a2451cbee37
pikaqiubaozou <1641196@qq.com>
2024-03-26 22:03:01 +0800

    EXERCISE

--- 01-arrays/src/main/java/oy/tol/tra/Grades.java
+++ 01-arrays/src/main/java/oy/tol/tra/Grades.java
@@ -31,13 +31,9 @@ public class Grades {
        5. Fix the issue.
        6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java, as instructed in the readme file.
       */
-      int i = 0;
-      while (i < grades.length/2) {
-         int temp = grades[i];
-         grades[i] = grades[grades.length - i - 1];
-         grades[grades.length - i - 1] = temp;
-         i++;
-      }
+
+
+      Algorithms.reverse(grades);
    }
 
    /**
@@ -52,19 +48,11 @@ public class Grades {
     5. Fix the issue.
     6. Transform the algorithm to <strong>use</strong> the generic one from Algorithms.java as instructed in the readme file.
    */
-      boolean sorted = false;
-      while (!sorted) {
-         sorted = true;
-         for (int i = 1; i < grades.length; i++) {
-            if (grades[i] < grades[i - 1]) {
-               int tmp = grades[i];
-               grades[i] = grades[i - 1];
-               grades[i - 1] = tmp;
-               sorted = false;
-            }
-         }
-      }
-   }
+
+
+
+         Algorithms.sort(grades);
+                 }
 
    /**
     * Returns the plain Java int [] array for investigation.
